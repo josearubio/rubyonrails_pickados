ee0b186767bb0ecbe83c7056f5b6cff0d050ca7a
Jaroboss <jaromuse@gmail.com>
2015-07-26 23:37:17 +0200

    ACCOUNTACTIVATION working BUT FLASH now fails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Bienvenido a Pickados !"
-
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "noreply@example.com"
+  layout '../user_mailer/account_activation'
+end
