a9cc66938500e29c499930c3c17e33448cbf4073
Jaroboss <jaromuse@gmail.com>
2015-07-28 19:25:15 +0200

    PASSWORD RESET OK. Sending emails from server, must send from real mail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "noreply@example.com"
-  layout '../user_mailer/account_activation'
+  #layout '../user_mailer/account_activation'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150726191855) do
+ActiveRecord::Schema.define(version: 20150728154718) do
 
   create_table "picks", force: true do |t|
     t.string   "pronostico"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20150726191855) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
