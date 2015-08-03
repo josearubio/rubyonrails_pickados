621bb09fafaa95fd44f827fc96b73060485dc567
Jaroboss <jaromuse@gmail.com>
2015-08-04 00:35:28 +0200

    Pagination working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,13 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
   before_action :correct_user,   only: [:edit, :update]
 
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def show
     @user = User.find(params[:id])
+    @picks = @user.picks.paginate(page: params[:page])
   end
 
   def new
