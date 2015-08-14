a47e7970b84d5377dd770496f13bb39185d52e0d
Jaroboss <jaromuse@gmail.com>
2015-08-14 20:35:24 +0200

    Picks have now status and result

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -4,4 +4,11 @@ class Pick < ActiveRecord::Base
   validates :user_id, presence: true
 	validates :evento, presence: true ,length: { maximum: 75 }
 	validates :pronostico, presence: true
+
+  def setstarted
+    update_attribute(:status,'started')
+  end
+  def setticked(result)
+    update_attribute(:status,'ticked')
+  end
 end
