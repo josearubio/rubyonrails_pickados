7bec19e659f48673f29d74c03386e0e7295be5a9
Jaroboss <jaromuse@gmail.com>
2015-08-30 00:12:10 +0200

    fixing some styles and backgrounds

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,24 @@
 class SessionsController < ApplicationController
   def new
   end
+
   def create
+
+    #Primero obtenemos el usuario a través del email
     user = User.find_by(email: params[:session][:email].downcase)
+    #Ahora comprobamos si la contraseña es la correcta con authenticate
     if user && user.authenticate(params[:session][:password])
-      if user.activated?
+
+      if user.activated? #Si el usuario ya está activado, nos logueamos
+        #Asignamos la id del user al objeto session
         log_in user
+        #Depende si hemos marcado la casilla recordar, setteamos la cookie o la borramos
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        #Redireccionamos a página de usuario
         redirect_back_or user
-      else
-        message  = "Account not activated. "
-        message += "Check your email for the activation link."
+      else #Si no está activado mostramos el mensaje de aviso.
+        message  = "Cuenta requiere activación. "
+        message += "Comprueba el email para el link de activación."
         flash[:warning] = message
         redirect_to root_url
       end
@@ -19,6 +27,7 @@ class SessionsController < ApplicationController
       render 'new'
     end
   end
+
   def destroy
     log_out if logged_in?
     redirect_to root_url
